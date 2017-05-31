9392b9bf01a50380753b823e492d1f9d2977b3fa
Sandi Rosyandi <sandi.rosyandi@aitindo.com>
2017-05-31 23:48:20 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'build.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: [
+                path.resolve(__dirname, 'node_modules')
+            ]
+        }, {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }]
+    },
+    watch: true
+}

be032f9ba05bd233d8e9d06411cadb70f7e651be
Sandi Rosyandi <sandi.rosyandi@aitindo.com>
2017-06-02 23:49:27 +0700

    Add methods

--- src/VueAwesomplete.vue
+++ src/VueAwesomplete.vue
@@ -13,22 +13,9 @@
                     return {
                         callback: function(){},
                         method: 'GET',
-                        url: ''
+                        url: null
                     }
                 }
-            },
-
-            close: Function,
-
-            highlight: Function,
-
-            open: Function,
-
-            select: Function,
-
-            selectComplete: {
-                type: Function,
-                default: function(){}
             },
 
             setting: {
@@ -43,7 +30,46 @@
 
         data() {
             return {
-                internalValue: ''
+                awesomplete: null,
+                internalValue: null
+            }
+        },
+
+        methods: {
+            close() {
+                this.awesomplete.close()
+            },
+
+            destroy() {
+                this.awesomplete.destroy()
+            },
+
+            evaluate() {
+                this.awesomplete.evaluate()
+            },
+
+            goto(index) {
+                this.awesomplete.goto(index)
+            },
+
+            list(list) {
+                this.awesomplete.list = list
+            },
+
+            next() {
+                this.awesomplete.next()
+            },
+
+            open() {
+                this.awesomplete.open()
+            },
+
+            previous() {
+                this.awesomplete.previous()
+            },
+
+            select() {
+                this.awesomplete.select()
             }
         },
 
@@ -51,28 +77,40 @@
             let self = this
             let input = this.$el
 
-            if (this.ajax.url == '') {
-                new Awesomplete(input, this.setting)
-            } else {
+            if (this.ajax.url) {
                 let ajax = new XMLHttpRequest();
                 ajax.open(this.ajax.method, this.ajax.url, true);
                 ajax.onload = function() {
                     let response = JSON.parse(ajax.responseText)
                     let list = self.ajax.callback(response)
                     self.setting.list = list
-                    new Awesomplete(input, self.setting)
+                    self.awesomplete = new Awesomplete(input, self.setting)
                 };
                 ajax.send()
+            } else {
+                this.awesomplete = new Awesomplete(input, this.setting)
             }
 
-            input.addEventListener('awesomplete-select', this.select)
+            input.addEventListener('awesomplete-select', () => {
+                this.$emit('select')
+            })
+
             input.addEventListener('awesomplete-selectcomplete', () => {
                 self.internalValue = input.value
-                this.selectComplete()
+                this.$emit('selectcomplete')
             })
-            input.addEventListener('awesomplete-open', this.open)
-            input.addEventListener('awesomplete-close', this.close)
-            input.addEventListener('awesomplete-highlight', this.highlight)
+
+            input.addEventListener('awesomplete-open', () => {
+                this.$emit('open')
+            })
+
+            input.addEventListener('awesomplete-close', () => {
+                this.$emit('close')
+            })
+
+            input.addEventListener('awesomplete-highlight', () => {
+                this.$emit('highlight')
+            })
         },
 
         watch: {
